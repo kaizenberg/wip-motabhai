be7e9a57d1a0eed6b9bee223a1cccd1998652cff
kaizenberg <kaizenberg.inbox@gmail.com>
2021-07-10 17:34:56 +0530

    Added newer files
    Converting to nodejs

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const app = express();
+
+app.use(compression());
+
+function handlerRender(req, res) {    
+    res.sendFile('index.html', {root: path.join(__dirname, 'assets')});
+}
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.get('*', handlerRender);
+app.listen(3000);
